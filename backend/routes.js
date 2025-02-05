ccb8a05bfcef02c809d96ef916c5f8b1525ba81e
Madeleine Lorås <madeleine.loras@gmail.com>
2025-02-05 11:20:57 +0100

    Rydding i variabler og struktur

--- backend/routes.js
+++ backend/routes.js
@@ -5,23 +5,24 @@ const games = [
   {
     name: "The Legend of Zelda: Breath of the Wild",
     year: 2017,
-    id: 1,
     company: "Nintendo",
     type: "Action-adventure",
     console: ["Nintendo Switch", "Wii U"],
+    minPlayer: 1,
+    maxPlayer: 1,
   },
   {
     name: "Cribbage",
     year: 0,
-    id: 6,
     company: "??",
     type: "Cardbased boardgame",
     console: ["Tabletop, Travel"],
+    minPlayer: 2,
+    maxPlayer: 6,
   },
   {
     name: "The Witcher 3: Wild Hunt",
     year: 2015,
-    id: 2,
     company: "CD Projekt",
     type: "Action role-playing",
     console: [
@@ -30,20 +31,22 @@ const games = [
       "Nintendo Switch",
       "Microsoft Windows",
     ],
+    minPlayer: 1,
+    maxPlayer: 1,
   },
 
   {
     name: "Baldur's Gate 3",
     year: 2020,
-    id: 3,
     company: "Larian Studios",
     type: "Role-playing",
     console: ["Microsoft Windows", "Stadia"],
+    minPlayer: 1,
+    maxPlayer: 2,
   },
   {
     name: "It Takes Two",
     year: 2021,
-    id: 4,
     company: "Hazelight Studios",
     type: "Action-adventure, platformer, cooperative",
     console: [
@@ -53,11 +56,15 @@ const games = [
       "Xbox Series X/S",
       "Microsoft Windows",
     ],
+    minPlayer: 2,
+    maxPlayer: 2,
   },
 ];
 
-router.get("/home", (req, res) => {
-  res.send("Velkommen til mitt spillbibliotek.");
+router.get("/", (req, res) => {
+  res.send(
+    "Velkommen til mitt spillbibliotek. For oversikt over alle spill gå til /games. For en liste med spill gå til /nameofgames."
+  );
 });
 
 //Get all games with all information
@@ -87,13 +94,18 @@ router.post("/newGame", (req, res) => {
   );
 });
 
-let newGame = {
-  name: "Scout",
-  year: 2024,
-  id: 9,
-  company: "Oink games",
-  type: "Cardbased boardgame",
-  console: ["Tabletop", "Travel"],
-};
+//Body for httpie
+// {
+//   "name": "Scout!",
+//   "releaseYear": 2022,
+//   "gameCompany": "Oink Games",
+//   "gameType": "Cardbased boardgame",
+//   "gameConsole": ["Tabletop", "Travel"],
+//   "maxPlayer": 6,
+//   "minPlayer": 2
+// }
+
+//Curl
+// curl -X POST http://localhost:3000/newGame \-H "Content-Type: application/json" \-d '{"name": "Scout!",  "releaseYear": 2022, "gameCompany": "Oink Games", "gameType": "Cardbased boardgame", "gameConsole": ["Tabletop", "Travel"], "maxPlayer": 6, "minPlayer": 2 }'
 
 module.exports = router;
